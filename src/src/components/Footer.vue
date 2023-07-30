993b707bef8f1bf386c5b474960b7b58b42b7ae4
IbnMarDuke <loveinda4a@gmail.com>
2023-07-30 10:19:58 +0300

    This is the commit test

--- src/src/components/Footer.vue
+++ src/src/components/Footer.vue
@@ -1,31 +1,47 @@
 <template>
   <footer>
-    <div style="z-index: 2;">
-      <div class="content">
-        <div class="MuiGrid-root MuiGrid-container css-1d3bbye">
-          <div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-lg-3 css-vgm7p6">
-            <div style="padding: 1rem;">
-              <div style="margin-bottom: 1.5rem;">
-                <div id="footer-brand">
-                  <img src="/src/assets/logo.svg">
-                </div>
-                <address style="margin-bottom: 1.5rem;">
-                  <span class="MuiTypography-root MuiTypography-body1 d-block css-1ryrt0q" id="company-address-info">
-                    <span class="d-block">41 University Drive Suite 400,<br>Newtown, PA 18940</span>
-                    <span class="d-block">United States</span>
-                  </span>
-                  <span class="MuiTypography-root MuiTypography-body1 d-block css-1ryrt0q" id="company-email-info">Email:<a href="mailto:sales@bilumix.com" aria-label="Mail to sales@bilumix.com" style="margin-left: 1rem; color: white;">sales@bilumix.com</a></span>
-                </address></div></div></div>
-          <div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-lg-6 css-1ysfe0q">
-          <div class="MuiGrid-root MuiGrid-container css-1d3bbye">
-          <div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 css-kiyygt">
-          <div style="padding: 1rem;">
-          <h6 class="MuiTypography-root MuiTypography-subtitle1 Footer_listTitle__mbDpg css-1lz6236">Pages</h6>
-          <ul class="MuiList-root MuiList-padding css-1ontqvh"><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Shop</span></span></div></li><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Offers</span></span></div></li><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Manual</span></span></div></li><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Support</span></span></div></li></ul></div></div><div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 css-kiyygt"><div style="padding: 1rem;"><h6 class="MuiTypography-root MuiTypography-subtitle1 Footer_listTitle__mbDpg css-1lz6236">Products</h6><ul class="MuiList-root MuiList-padding css-1ontqvh"><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Headlamp</span></span></div></li><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Loupes</span></span></div></li><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Accessories</span></span></div></li><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Replaceable</span></span></div></li></ul></div></div><div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 css-kiyygt"><div style="padding: 1rem;"><h6 class="MuiTypography-root MuiTypography-subtitle1 Footer_listTitle__mbDpg css-1lz6236">Legal</h6><ul class="MuiList-root MuiList-padding css-1ontqvh"><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Refund Policy</span></span></div></li><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Data Privacy</span></span></div></li><li class="MuiListItem-root footer-nav-item MuiListItem-gutters MuiListItem-padding css-cq733n" style="padding: 0px;"><div class="MuiListItemText-root css-1tsvksn"><span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-1ox1c9e"><span class="footer-nav-text">Terms of Service</span></span></div></li></ul></div></div></div></div><div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-lg-3 css-vgm7p6"><div style="padding: 1rem;"><div><h6 class="MuiTypography-root MuiTypography-subtitle1 css-1lz6236" style="font-size: 20px;">Follow us</h6><div><button class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk" tabindex="0" type="button" style="color: white;"><a href="https://www.facebook.com/BiLumixheadlamp/" rel="noreferrer" target="_blank" aria-label="Connect with our facebook" style="margin-right: 15px; color: white;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FacebookIcon"><path d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z"></path></svg></a><span class="MuiTouchRipple-root css-w0pj6f"></span></button><button class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-1yxmbwk" tabindex="0" type="button" style="color: white;"><a href="https://www.instagram.com/bilumix/?hl=en" rel="noreferrer" target="_blank" aria-label="Connect with our instagram" style="margin-right: 15px; color: white;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="InstagramIcon"><path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path></svg></a><span class="MuiTouchRipple-root css-w0pj6f"></span></button></div></div></div></div></div><div class="MuiTypography-root MuiTypography-caption css-1v2gfp5" style="line-height: 20px; text-align: center; color: rgba(255, 255, 255, 0.5);">Copyright © 2021 BiLumix.com Designed and built by Omiologic LLC
+    <div>
+      <div>
+        <div>
+          <div>
+            <img src="/src/assets/logo.svg">
+          </div>
+          <address>
+            <span>41 University Drive Suite 400,<br>Newtown, PA 18940<br>United States</span>
+            <span>Email:<a href="mailto:sales@bilumix.ru" aria-label="Mail to sales@bilumix.ru" style="margin-left: 1rem; color: white;">sales@bilumix.ru</a></span>
+          </address></div></div></div>
+
+
+    <div>
+      <div style="padding: 1rem;">
+        <h6>Pages</h6>
+        <ul><li>
+          <div><a href="/shop/">Shop</a></div>
+        </li>
+          <li style="padding: 0px;">
+            <div><a href="/offers/">Offers</a></div>
+          </li><li style="padding: 0px;">
+            <div><a href="/manual/">Manual</a></div>
+          </li><li style="padding: 0px;">
+            <div><a href="/support/">Support</a></div>
+          </li></ul>
+        <div><div><h6><a href="/products/">Products</a></h6>
+          <ul><li>
+            <div><a href="/headlamp/">Headlamp</a></div></li>
+            <li><div><a href="/loupes/">Loupes</a></div></li>
+            <li><div><a href="/accessories/">Accessories</a></div></li>
+            <li><div><a href="/replaceable/">Replaceable</a></div></li></ul></div>
+        </div>
+        <div><div><h6><a href="/legal/">Legal</a></h6>
+          <ul><li><div><a href="refund-policy/">Refund Policy</a></div></li>
+            <li><div><a href="/data-privacy/">Data Privacy</a></div></li>
+            <li><div><a href="/terms-of-service/">Terms of Service</a></div></li></ul></div></div>
+        <div>Copyright © 2021 BiLumix.com Designed and built by Omiologic LLC</div>
       </div>
-      </div>
+
     </div>
   </footer>
+
 </template>
 
 <script lang="ts">
@@ -48,9 +64,21 @@ export default defineComponent({
 </script>
 
 <style scoped>
+div {
+  display: block;
+  flex-direction: row;
+}
+*, ::before, ::after {
+  box-sizing: inherit;
+}
 footer {
   background-color: #151515;
   color: hsla(0,0%,100%,.8);
+  font-family: Roboto, sans-serif;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 1.5;
+  letter-spacing: 0.00938em;
   display: -webkit-flex;
   display: -moz-box;
   display: flex;
@@ -63,6 +91,9 @@ footer {
   box-shadow: 0 2px 4px -1px rgba(0,0,0,.2),0 4px 5px 0 rgba(0,0,0,.14),0 1px 10px 0 rgba(0,0,0,.12);
   padding: 4rem 0 1rem;
   position: relative;
+}
+address {
+  font-style: italic;
 }
 footer .content{
   margin: 10px 80px;
@@ -77,4 +108,5 @@ footer ul{
   padding: 0;
   list-style: none;
 }
+
 </style>
